4de342f5bbb66e9e5e83d7e89b4047fc4fb71d5b
hakanfremin <hakan.fremin@gmail.com>
2026-04-17 12:17:17 +0000

    Add Codespace/localhost API URL support and REST endpoint root

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -17,7 +17,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
+
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-*4xq+wkc=gkkl-+i5!ew5+63wv1qv1@9e^l1f$zs%@x=r$g%b_'
@@ -25,7 +27,14 @@ SECRET_KEY = 'django-insecure-*4xq+wkc=gkkl-+i5!ew5+63wv1qv1@9e^l1f$zs%@x=r$g%b_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+"""
+Support Codespace and localhost URLs
+"""
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
+ALLOWED_HOSTS.append('*')
 
 
 # Application definition
